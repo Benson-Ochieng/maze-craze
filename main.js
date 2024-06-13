5ea7a6ba5ea07b517239e35665f5796fd014b95e
Benson Ochieng <markbenson10368@gmail.com>
2024-06-13 11:13:44 +0300

    Working with matter js creating shapes

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-const { Engine, Render, Runner, World, Bodies } = Matter;
+const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter;
+
+const width = 800;
+const height = 600;
 
 const engine = Engine.create();
 const { world } = engine;
@@ -6,14 +9,41 @@ const render = Render.create({
     element: document.body,
     engine: engine,
     options: {
-        width: 800,
-        height: 600
+        wireframes: false,
+        width: width,
+        height: height
     }
 });
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-const shape = Bodies.rectangle(200, 200, 50, 50, {
-    isStatic: true
-});
-World.add(world, shape);
+World.add(world, MouseConstraint.create(engine, {
+    mouse: Mouse.create(render.canvas)
+}));
+
+//Walls
+const walls = [
+    Bodies.rectangle(400, 0, 800, 40, { isStatic: true}) ,//Top Wall
+    Bodies.rectangle(400, 600, 800, 40, { isStatic: true}), // Bottom Wall
+    Bodies.rectangle(0, 300, 40, 600, { isStatic: true}), //Left Wall
+    Bodies.rectangle(800, 300, 40, 600, { isStatic: true}) //Right Wall
+];
+World.add(world, walls);
+
+//Random shapes
+
+
+
+for(let i = 0; i < 50; i++){
+    if (Math.random() > 0.5) {
+        World.add(world, Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50, ));
+    }else{
+        World.add(world, Bodies.circle(Math.random() * width, Math.random() * height, 35, {
+            render: {
+                fillStyle: 'blue'
+            }
+        }));
+    }
+};
+
+
